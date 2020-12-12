accb1bf9e4839794100885ce98531f6db42357de
eddymandran <dev.eddy.mandran@gmail.com>
2020-12-12 14:57:47 +0100

    debut challenge quete

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 // index.js
 const app = require('./app');
 
-const port = process.env.port || 3000;
+const port = process.env.port || 3001;
 
-app.listen(3000, (err) => {
+app.listen(3001, (err) => {
   if (err) {
     throw new Error(`An error occurred: ${err.message}`);
   }
   console.log(`Server is listening on ${port}`);
 });
-
--- test/app.integration.spec.js
+++ test/app.integration.spec.js
@@ -1,8 +1,11 @@
 // test/app.integration.spec.js
 const request = require('supertest');
 const app = require('../app');
+const connection = require('../connection');
 
 describe('Test routes', () => {
+  // truncate bookmark table before each test
+  beforeEach((done) => connection.query('TRUNCATE bookmark', done));
   it('GET / sends "Hello World" as json', (done) => {
     request(app)
       .get('/')
@@ -14,4 +17,44 @@ describe('Test routes', () => {
         done();
       });
   });
+  it('POST /bookmarks - error (fields missing) ', (done) => {
+    request(app)
+      .post('/bookmarks')
+      .send({})
+      .expect(422)
+      .expect('Content-Type', /json/)
+      .then((response) => {
+        const expected = { error: 'required field(s) missing' };
+        expect(response.body).toEqual(expected);
+        done();
+      });
+  });
+  it('POST /bookmarks - OK (fields provided) ', (done) => {
+    request(app)
+      .post('/bookmarks')
+      .send({ url: 'https://jestjs.io', title: 'Jest' })
+      .expect(201)
+      .expect('Content-Type', /json/)
+      .then((response) => {
+        const expected = {
+          id: expect.any(Number),
+          url: 'https://jestjs.io',
+          title: 'Jest',
+        };
+        expect(response.body).toEqual(expected);
+        done();
+      })
+      .catch(done);
+  });
+});
+
+describe('GET /bookmarks/:id', () => {
+  const testBookmark = { url: 'https://nodejs.org/', title: 'Node.js' };
+  beforeEach((done) =>
+    connection.query('TRUNCATE bookmark', () =>
+      connection.query('INSERT INTO bookmark SET ?', testBookmark, done)
+    )
+  );
+
+  // Write your tests HERE!
 });
